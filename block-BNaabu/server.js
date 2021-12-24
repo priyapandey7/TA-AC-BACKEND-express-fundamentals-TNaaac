cca5c6ca9f44362a16c8d87b7a0fdff37cf8d8c8
priya pandey <priyarock746@gmail.com>
2021-12-24 20:21:47 +0530

    Refactor

--- block-BNaabu/server.js
+++ block-BNaabu/server.js
@@ -14,19 +14,18 @@ app.get('/admin',(req,res,next) =>{
 app.get('/',(req,res) => {
     res.send('Welcome')
 })
-
 // - GET request on '/about'
-app.get('/user',(req,res) => {
-    res.send('Users page')
+app.get('/about',(req,res) => {
+    res.send('About page')
 })
 // throw 404
-app.use((req,res,next) =>{
+app.use((req,res,next) => {
     res.send('Page not found')
-})
+});
 
-// middleware which throws err
+//custom middleware which throws err
 app.use((err,req,res,next) =>{
-  res.send(err)
+ res.status(500).send(err);
 })
 
 app.listen(3000, () => {
@@ -34,44 +33,3 @@ app.listen(3000, () => {
 });
 
 
-
-
-
-
-
-// var express = require('express')
-// var app = express();
-// var logger = require('morgan')
-
-// app.get('/',(req,res) => {
-//     res.send('Welcome')
-// })
-
-// // miiddleware
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
-// app.use(express.static(__dirname + "/public"));
-// app.use(logger('dev'))
-// // index route
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
-// // new route
-// app.get('/new', (req, res) => {
-//     res.sendFile(__dirname + "/new.html");
-// });
-// // post req 
-// app.post('/new', (req, res) => {
-//     res.json(req.body);
-// });
-
-// // 
-// app.get('/users/:username',(req,res) => {
-//     var username = req.params.username;
-//     res.send(username)
-// })
-
-
-// app.listen(3000, () => {
-//     console.log('server is listeing on port 3000');
-// });
